844791882e19077f1bf0e8532954339f91fd9ba4
MinhPN1021 <minh.ngo@dawsoncollege.qc.ca>
2023-02-17 11:39:19 -0500

    decrypt function

--- ND Nerd Tech/NT2.py
+++ ND Nerd Tech/NT2.py
@@ -4,6 +4,36 @@
 CONSOLE_LANGUAGE = "ENG"          
 #DO NOT DELETE STATEMENT / NE PAS EFFACER CETTE DÉCLARATION
 
+Morse_Code = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+
+def decrypt(message: str) -> str :
+    word=message.split("/")
+    result=''
+    for character in word:
+        result+=" "
+        letter=character.split()
+        for c in letter:
+            for keys in Morse_Code.keys():
+                if c == Morse_Code[keys]:
+                    result+=keys.lower()
+    return result
+    
+    
 
 
 def solve(testCase):
