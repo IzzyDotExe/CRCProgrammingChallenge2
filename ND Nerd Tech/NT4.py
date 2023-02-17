fff97b3f4f2e983b39938b8d92d8c90965b7fa09
Israel Aristide <44146685+IsraelAristide@users.noreply.github.com>
2023-02-17 12:45:27 -0500

    nt4 added

--- ND Nerd Tech/NT4.py
+++ ND Nerd Tech/NT4.py
@@ -5,24 +5,131 @@ CONSOLE_LANGUAGE = "ENG"
 #DO NOT DELETE STATEMENT / NE PAS EFFACER CETTE DÉCLARATION
 
 
-
 def solve(testCase):
     joueur = testCase[0]
     tireurs = testCase[1]
     murs = testCase[2]
     # Code goes here / Votre code commence ici 
     
+    # NOTE: I believe test 1 answer might be wrong
+    
+    class Point:
+        
+        def __init__(self, x, y):
+            self.x = round(x, 5)
+            self.y = round(y, 5)
+        
+        def __repr__(self):
+            return f"({self.x}, {self.y})"
+            
+    class Player:
+        
+        def __init__(self, pos):
+            self.position = pos
+    
+    class Line:
+        
+        def __init__(self, p1, p2):
+            self.points = [p1, p2] 
+
+        def get_slope(self):
+            p1 = self.points[0]
+            p2 = self.points[1]
+            try:
+                return (p2.y - p1.y) / (p2.x - p1.x)
+            except ZeroDivisionError:
+                return 0
+             
+        def get_origin(self):
+            p1 = self.points[0]
+            slope = self.get_slope()
+            return p1.y - slope * p1.x   
+    
+        def evalulate_y(self, x):
+            slope = self.get_slope()
+            origin = self.get_origin()
+            return  round(slope * x + origin, 5)
+
+        def get_intersect_point(self, line2):
+            slope = self.get_slope()
+            origin = self.get_origin()
+            slope2 = line2.get_slope()
+            origin2 = line2.get_origin()
+            
+            try:
+                x = (origin2 - origin) / (slope - slope2)
+            except ZeroDivisionError:
+                return None
+            
+            y = self.evalulate_y(x)
+            y2 = line2.evalulate_y(x)
+            
+            if y == y2:
+                return Point(x, y)
+            else:
+                return None
+
+        def get_intersect_point_precise(self, line2):
+            
+            intersect = self.get_intersect_point(line2)
+            
+            if intersect is not None:
+                xrange = [self.points[0].x, self.points[1].x]
+                xrange2 = [line2.points[0].x, line2.points[1].x]
+                
+                xrange.sort()
+                xrange2.sort()
+                
+                yrange = [self.points[0].y, self.points[1].y]
+                yrange2 = [line2.points[0].y, line2.points[1].y]
+                
+                yrange.sort()
+                yrange2.sort()
+                
+                if intersect.y >= yrange[0] and intersect.y <= yrange[1] and \
+                   intersect.y >= yrange2[0] and intersect.y <= yrange2[1]:
+                    return intersect
+            
+            return None
+        
+
+    enemies = []
+    walls = []
+    walls.append(Line(Point(0, 0), Point(0, 10)))
+    walls.append(Line(Point(0, 0), Point(10, 0)))
+    walls.append(Line(Point(10, 10), Point(0, 10)))
+    walls.append(Line(Point(10, 10), Point(10, 0)))
+    
+    player = Player(Point(joueur[0], joueur[1]))
+    
+    for x in tireurs:
+        enemies.append(Player(Point(x[0], x[1])))
+        
+    for x in murs:
+        walls.append(Line(Point(x[0], x[1]), Point(x[2], x[3])))
+    
+    sightlines = []
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    return     #Return test case output here / Retourner la sortie de votre code sur cette ligne
+    for x in enemies:
+        
+        sightline = Line(player.position, x.position)
+        intersects = False
+        for w in walls:
+            
+            if sightline.get_intersect_point_precise(w):
+                intersects = True
+                break
+            
+        if not intersects:
+            sightlines.append((enemies.index(x), sightline))            
+            
+            
+    if sightlines:
+        return ",".join([str(x[0]) for x in sightlines])
+
+    return "SAFE"  #Return test case output here / Retourner la sortie de votre code sur cette ligne
 
 
 
